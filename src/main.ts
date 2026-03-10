d3d0ab37000bfcdf048fcc2a9280cff02691e090
Asiah Crutchfield <195882349+asiahcrutchfield@users.noreply.github.com>
2026-03-10 21:45:54 +0800

    Add submodule

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import "./style.css"
 // getting paths to stories, images and audio
 let storyLang: string = "zh"
 let level: string = "0"
-const imgPath: string = `/old_dictionary/${storyLang}/images/`
-const vocabAudioPath: string = `/old_dictionary/${storyLang}/audio/lvl${level}/vocab/`
-const storyAudioPath: string = `/old_dictionary/${storyLang}/audio/lvl${level}/stories/`
-const storyPath: string = `/old_dictionary/${storyLang}/levels/lvl${level}/stories.json` // path to stories
-const vocabPath: string = `/old_dictionary/${storyLang}/levels/lvl${level}/lvl${level}.json` // path to vocab list
+const imgPath: string = `/dictionary/${storyLang}/images/`
+const vocabAudioPath: string = `/dictionary/${storyLang}/audio/lvl${level}/vocab/`
+const storyAudioPath: string = `/dictionary/${storyLang}/audio/lvl${level}/stories/`
+const storyPath: string = `/dictionary/${storyLang}/levels/lvl${level}/stories.json` // path to stories
+const vocabPath: string = `/dictionary/${storyLang}/levels/lvl${level}/lvl${level}.json` // path to vocab list
 
 // defining types
 type Story = {
